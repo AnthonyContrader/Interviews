b5bd3867cf357679d867ae39356a67bbe48a48f1
fabrizio <frizio@hotmail.it>
2019-10-08 10:38:24 +0200

    restored converter interface and abstract class

--- src/main/java/it/contrader/converter/AbstractConverter.java
+++ src/main/java/it/contrader/converter/AbstractConverter.java
@@ -3,9 +3,6 @@ package it.contrader.converter;
 import java.util.ArrayList;
 import java.util.List;
 
-import it.contrader.dto.UserDTO;
-import it.contrader.model.User;
-
 /**
  * Questa classe implementa i metodi che convertono le liste.
  *
@@ -17,26 +14,8 @@ import it.contrader.model.User;
  * @see Converter
  */
 public abstract class AbstractConverter<Entity,DTO> implements Converter<Entity,DTO> {
-	public User toEntity(UserDTO userDTO) {
-		User user = null;
-		if (userDTO != null) {
-			user = new User(userDTO.getId(),userDTO.getUsername(),userDTO.getPassword(),userDTO.getUserType(),userDTO.getEmail());			
-		}
-		return user;
-	}
-	
-	public UserDTO toDTO(User user) {
-		UserDTO userDTO = null;
-		if (user != null) {
-			userDTO = new UserDTO(user.getId(),user.getUsername(),user.getPassword(),user.getUserType(),user.getEmail());
-			
-		}
-		return userDTO;
-	}
-	
 	public List<Entity> toEntityList (Iterable<DTO> listDTO) {
 		List<Entity> list = new ArrayList<Entity>();
-
 		if(listDTO != null) {
 			for (DTO dto:listDTO) {
 				Entity entity = toEntity(dto);
@@ -48,7 +27,6 @@ public abstract class AbstractConverter<Entity,DTO> implements Converter<Entity,
 	
 	public List<DTO> toDTOList (Iterable<Entity> listEntity) {
 		List<DTO> list = new ArrayList<DTO>();
-
 		if(listEntity != null) {
 			for (Entity entity:listEntity) {
 				DTO dto = toDTO(entity);
--- src/main/java/it/contrader/converter/UserConverter.java
+++ src/main/java/it/contrader/converter/UserConverter.java
@@ -16,4 +16,19 @@ import it.contrader.model.User;
  */
 @Component
 public class UserConverter extends AbstractConverter<User,UserDTO> {
+	public User toEntity(UserDTO userDTO) {
+		User user = null;
+		if (userDTO != null) {
+			user = new User(userDTO.getId(),userDTO.getUsername(),userDTO.getPassword(),userDTO.getUserType(),userDTO.getEmail());			
+		}
+		return user;
+	}
+	public UserDTO toDTO(User user) {
+		UserDTO userDTO = null;
+		if (user != null) {
+			userDTO = new UserDTO(user.getId(),user.getUsername(),user.getPassword(),user.getUserType(),user.getEmail());
+			
+		}
+		return userDTO;
+	}
 }
