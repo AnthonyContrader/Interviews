bf00f11acf1a0608b6856dbe91c912282a95f35d
Giuseppe Pitasi <giuseppe.pitasi@gmail.com>
2019-10-09 15:15:11 +0200

    recruiter management

--- src/app/admin/recruiters/recruiters.component.ts
+++ src/app/admin/recruiters/recruiters.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { RecruiterDTO } from 'src/dto/recruiterdto';
+import { RecruiterService } from 'src/service/recruiter.service';
+import { CompanyDTO } from 'src/dto/companydto';
+import { CompanyService } from 'src/service/company.service';
 
 @Component({
   selector: 'app-recruiters',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecruitersComponent implements OnInit {
 
-  constructor() { }
+  recruiters: RecruiterDTO[];
+  companies: CompanyDTO[];
+  recruitertoinsert: RecruiterDTO = new RecruiterDTO();
+
+  constructor(private service: RecruiterService, private companyService: CompanyService) { }
 
   ngOnInit() {
+    this.getRecruiters();
+  }
+
+  getRecruiters() {
+    this.service.getAll().subscribe(recruiters => this.recruiters = recruiters);
+    this.companyService.getAll().subscribe(companies => this.companies = companies);
+  }
+
+  delete(recruiter: RecruiterDTO) {
+    this.service.delete(recruiter.id).subscribe(() => this.getRecruiters());
+  }
+
+  update(recruiter: RecruiterDTO) {
+    this.service.update(recruiter).subscribe(() => this.getRecruiters());
+  }
+
+  insert(recruiter: RecruiterDTO) {
+    this.service.insert(recruiter).subscribe(() => this.getRecruiters());
+    this.clear();
+  }
+
+  clear() {
+    this.recruitertoinsert = new RecruiterDTO();
   }
 
 }
