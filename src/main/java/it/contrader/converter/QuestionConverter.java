f03ec8881f0f30ee49d4fcf9ea4ad5e8d85ad7d2
fabrizio <frizio@hotmail.it>
2019-10-10 13:05:59 +0200

    added question

--- src/main/java/it/contrader/converter/QuestionConverter.java
+++ src/main/java/it/contrader/converter/QuestionConverter.java
@@ -8,9 +8,16 @@ import it.contrader.model.Question;
 @Component
 public class QuestionConverter extends AbstractConverter<Question, QuestionDTO> {
 	public QuestionDTO toDTO (Question question) {
-		return new QuestionDTO (question.getId(), question.getQuestion(), question.getTopic(), question.getSector(), question.getRecruiter(), question.getCompany());
+		QuestionDTO questionDTO=null;
+				if(question!=null ) {
+				}
+				return new QuestionDTO (question.getId(), question.getQuestion(), question.getTopic(), question.getSector(), question.getRecruiter(), question.getCompany());
 	}
+	
 	public Question toEntity (QuestionDTO questionDTO) {
+		Question question=null;
+		if(questionDTO!=null) {
+		}
 		return new Question (questionDTO.getId(), questionDTO.getQuestion(), questionDTO.getTopic(), questionDTO.getSector(), questionDTO.getRecruiter(), questionDTO.getCompany());
 	}
 }
--- src/main/java/it/contrader/dto/QuestionDTO.java
+++ src/main/java/it/contrader/dto/QuestionDTO.java
@@ -1,5 +1,8 @@
 package it.contrader.dto;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import it.contrader.model.Company;
 import it.contrader.model.Recruiter;
 import lombok.AllArgsConstructor;
@@ -9,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
 public class QuestionDTO {
 	private long id;
 	private String question;
